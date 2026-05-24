c19830048b16d10ba9adeba6f56b89383f435530
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-05-24 12:34:55 +0530

    Update import path for StartupDiscoveryPage

--- src/app/startup/page.tsx
+++ src/app/startup/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import StartupDiscoveryPage from '@/components/StartupHub';
+import StartupDiscoveryPage from '../../components/StartupHub';
 //import { motion } from 'framer-motion';
 import {
   Rocket,
