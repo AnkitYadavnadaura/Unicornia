de662b1605a191e40b7daf4fdb8b7705925f99d4
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-05-24 12:37:45 +0530

    Update page.tsx

--- src/app/startup/page.tsx
+++ src/app/startup/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react';
-import StartupDiscoveryPage from '../../components/startupHub';
+import StartupDiscoveryPage from '../../components/startuphub';
 //import { motion } from 'framer-motion';
 import {
   Rocket,
