01cafb9e4a71bb7a01a495fcf0d890619774df12
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-05-25 22:31:15 +0530

    Update server.ts

--- src/app/lib/server.ts
+++ src/app/lib/server.ts
@@ -1,4 +1,8 @@
-import { createServerClient } from '@supabase/ssr'
+import {
+  createServerClient,
+  type CookieOptions,
+} from '@supabase/ssr'
+
 import { cookies } from 'next/headers'
 
 export async function createClient() {
@@ -12,10 +16,22 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+
+        setAll(
+          cookiesToSet: {
+            name: string
+            value: string
+            options: CookieOptions
+          }[]
+        ) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
+            cookiesToSet.forEach(
+              ({ name, value, options }) =>
+                cookieStore.set(
+                  name,
+                  value,
+                  options
+                )
             )
           } catch {}
         },
