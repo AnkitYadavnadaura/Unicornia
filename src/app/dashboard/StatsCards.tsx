0c927c2efd97632ed4bb55959ea29bf67a668e1b
Ankit yadav <49826590+AnkitYadavnadaura@users.noreply.github.com>
2026-05-28 13:45:55 +0530

    Update StatsCards.tsx

--- src/app/dashboard/StatsCards.tsx
+++ src/app/dashboard/StatsCards.tsx
@@ -1,3 +1,5 @@
+// StatsCards.tsx
+
 'use client'
 
 import {
@@ -32,6 +34,33 @@ const stats = [
 
 export default function StatsCards() {
   return (
-    <div className="grid grid-cols-4 gap-6 mt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mt-8">
       {stats.map((item) => {
+        const Icon = item.icon
+
+        return (
+          <div
+            key={item.title}
+            className="bg-white rounded-[30px] p-6 shadow-sm"
+          >
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="text-gray-500">
+                  {item.title}
+                </p>
+
+                <h3 className="text-5xl font-black mt-4">
+                  {item.value}
+                </h3>
+              </div>
+
+              <div className="w-16 h-16 rounded-2xl bg-[#f5f7fb] flex items-center justify-center">
+                <Icon />
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
